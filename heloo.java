b6b7a6ccb6adbcb00debe101cc8a425a5676a044
charulp26 <charulpatidar026@gmail.com>
2024-07-08 13:56:45 +0530

    Update heloo.java

--- heloo.java
+++ heloo.java
@@ -6,6 +6,7 @@ public static void main(String rags[])
 
    System.out.println("HellsaSdDemo");
   System.out.println("Done lets go q");
-      System.out.println("what is q")
+      System.out.println("what is q");
+        System.out.println("affifa q");
 
   }
