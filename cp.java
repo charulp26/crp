2d2a4340d935c82e78693d2d3a927832274f4e4e
charulp26 <charulpatidar026@gmail.com>
2024-07-08 14:31:06 +0530

    Create cp.java

---
+++ cp.java
@@ -0,0 +1,13 @@
+class hello
+  {
+
+public static void main(String rags[])
+    {
+
+   System.out.println("HellsaSdDemo");
+  System.out.println("Done lets go q");
+      System.out.println("what is q");
+        System.out.println("affifa q");
+              System.out.println("affifa abc");
+
+  }
